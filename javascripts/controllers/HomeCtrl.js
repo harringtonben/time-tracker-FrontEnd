8ce473acc18b4c3ac2c91c0341e6358fd063fcb2
ben harrington <harringtonben@icloud.com>
2018-06-09 11:19:30 -0500

    successfully calling data from my API and printing to the page

--- javascripts/controllers/HomeCtrl.js
+++ javascripts/controllers/HomeCtrl.js
@@ -1,5 +1,14 @@
 'use strict';
 
-app.controller("HomeCtrl", function($location, $scope) {
-   $scope.home = 'HOME';
+app.controller("HomeCtrl", function($location, $scope, HomeService) {
+   const getHomeMetrics = (timeframe) => {
+      HomeService.getHomeMetrics(timeframe).then((results) => {
+         $scope.teamMetrics = results.data;
+         console.log($scope.teamMetrics);
+      }).catch((error) => {
+         console.log(error);
+      });
+   };
+
+   getHomeMetrics(120);
 });
