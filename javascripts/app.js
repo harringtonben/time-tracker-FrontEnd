103fc4255e476fe42f280c590200c71a66545288
ben harrington <harringtonben@icloud.com>
2018-06-09 13:18:39 -0500

    have graph and data populating on page now

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,4 +2,5 @@
 
 'use strict';
 
-const app = angular.module("TimeTracker", ["ngAnimate", "ngRoute", "ngSanitize"]);
+const app = angular.module("TimeTracker", ["ngAnimate", "ngRoute", "ngSanitize", "chart.js"]);
+
--- javascripts/controllers/HomeCtrl.js
+++ javascripts/controllers/HomeCtrl.js
@@ -4,11 +4,33 @@ app.controller("HomeCtrl", function($location, $scope, HomeService) {
    const getHomeMetrics = (timeframe) => {
       HomeService.getHomeMetrics(timeframe).then((results) => {
          $scope.teamMetrics = results.data;
+          const employeeNames = $scope.teamMetrics.map(team => team.name);
+          const totalOutTime = $scope.teamMetrics.map(team => team.totalCalledOut);
+          const totalUnplannedTimeOut = $scope.teamMetrics.map(team => team.totalUnplannedOut);
+          const totalWorkFromHome = $scope.teamMetrics.map(team => team.totalWorkedFromHome);
          console.log($scope.teamMetrics);
+         console.log(employeeNames);
+         console.log(totalOutTime);
+         console.log(totalUnplannedTimeOut);
+          $scope.labels = employeeNames;
+          $scope.series = ['Total Time Out', 'Total Unplanned Time Out', 'Total Days Worked From Home'];
+
+          $scope.data = [
+              totalOutTime,
+              totalUnplannedTimeOut,
+              totalWorkFromHome
+          ];
+
+          $scope.colors = [ '#1E91D6', '#F08700', '#8FC93A', '#E4CC37', '#F06543', '#F9F871'];
       }).catch((error) => {
          console.log(error);
       });
    };
+
+    $(document).ready(function(){
+        $('.collapsible').collapsible();
+    });
+
 
    getHomeMetrics(120);
 });
