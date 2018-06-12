0c431891d0ccefd2e0c8ec28a9e231a6abed6c38
ben harrington <harringtonben@icloud.com>
2018-06-12 18:52:24 -0500

    finished writing delete functionality and now testing

---
+++ javascripts/controllers/Managers/DeleteManagerCtrl.js
@@ -0,0 +1,27 @@
+'use strict';
+
+app.controller("DeleteManagerCtrl", function($timeout, $location, $routeParams, $scope, ManagerService) {
+
+    const getManagerData = () => {
+        ManagerService.getManager($routeParams.id).then((results) => {
+            $scope.manager = results.data;
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
+
+    $scope.goHome = () => {
+        $location.path("/managers");
+    };
+
+    $scope.delete = () => {
+        ManagerService.deletManager($routeParams.id).then((results) => {
+            console.log(results);
+            $location.path("/managers");
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
+
+    getManagerData();
+});
